43ae06e7874bb7a5d97559325f1b9a847c2bf552
Duygu Genc <duygugenc@Duygus-MacBook-Pro.local>
2024-11-05 17:41:42 +0100

    auth views fixed & now auth is working as intended

--- prod/modules/auth_views/iam.tf
+++ prod/modules/auth_views/iam.tf
@@ -1,56 +1,55 @@
-data "google_iam_policy" "viewer"{
-  binding {
-    role = "roles/bigquery.dataViewer"
-    members = [ "serviceAccount:authorised-view-service-acc@data-domain-data-warehouse.iam.gserviceaccount.com" ]
-  }
+
+locals {
+  service_account = "authorised-view-service-acc@data-domain-data-warehouse.iam.gserviceaccount.com"
 }
 
-resource "google_project_iam_member" "project_permissions_fine_grained_reader" {
+resource "google_project_iam_member" "project_permissions_bq_user" {
   project    = var.project_id
-  role               = "roles/datacatalog.categoryFineGrainedReader" # Grant permission to use the service account
-  member = "serviceAccount:authorised-view-service-acc@data-domain-data-warehouse.iam.gserviceaccount.com"  # Grant access to the service account
-}
-resource "google_bigquery_dataset_iam_member" "dataset_permissions_vault_reader" {
-  dataset_id = "compilance_database"                          
-  project    = "sambla-group-compliance-db"  
-  role               = "roles/bigquery.dataViewer" # Grant permission to use the service account
-  member = "serviceAccount:authorised-view-service-acc@data-domain-data-warehouse.iam.gserviceaccount.com"  # Grant access to the service account
+  role               = "roles/bigquery.user" # Grant permission to use the service account
+  member =  "serviceAccount:${local.service_account}" 
 }
 
-
-# IAM Policy for Authorized Views
-resource "google_bigquery_table_iam_policy" "auth_view_iam_policy" {
+# IAM Policy for service account to read Authorized Views
+resource "google_bigquery_table_iam_member" "auth_view_iam_paccess" {
   for_each = local.schema_table_queries
   project    = var.project_id
   dataset_id = "authorized_view_test_${each.value.schema}" 
-  table_id   = "view_${each.value.table}"                   
-  policy_data = data.google_iam_policy.viewer.policy_data    
+  table_id   = "view_${each.value.table}"  
+  role = "roles/bigquery.dataViewer"
+  member =  "serviceAccount:${local.service_account}"                  
   depends_on = [google_bigquery_table.dynamic_auth_views] 
 }
 
-# Dataset Access for Authorized Views
-resource "google_bigquery_dataset_access" "auth_view_access_dataset"{
-  for_each = local.schema_table_queries
-  project = var.project_id
-  dataset_id = "${each.value.schema}"
-  view{
-    project_id = var.project_id
-    dataset_id = "authorized_view_test_${each.value.schema}"
-    table_id = "view_${each.value.table}" 
-  }
-}
-
 # Dataset Access for GDPR Vault
-resource "google_bigquery_dataset_access" "auth_view_access_to_gdpr_vault" {
-  for_each = local.schema_table_queries
+resource "google_bigquery_dataset_access" "auth_dataset_access_to_gdpr_vault" {
+  for_each = { for schema in local.unique_schemas : schema => schema }
 
   dataset_id = "compilance_database"                          
   project    = "sambla-group-compliance-db"                    
-
-  view {
-    project_id = var.project_id
-    dataset_id = "authorized_view_test_${each.value.schema}"   
-    table_id   = "view_${each.value.table}"                   
+  dataset {
+    dataset{
+      project_id = var.project_id
+      dataset_id = "authorized_view_test_${each.key}"  
+    }
+    target_types = ["VIEWS"]
   }
+
+  depends_on = [google_bigquery_table.dynamic_auth_views] 
+}
+
+# Dataset Access for legacy stack
+resource "google_bigquery_dataset_access" "auth_view_access_legacy_dataset" {
+  for_each = { for schema in local.unique_schemas : schema => schema }
+
+  dataset_id = "${each.key}"                          
+  project    = var.project_id                   
+  dataset {
+    dataset{
+      project_id = var.project_id
+      dataset_id = "authorized_view_test_${each.key}"  
+    }
+    target_types = ["VIEWS"]
+  }
+
   depends_on = [google_bigquery_table.dynamic_auth_views] 
 }
