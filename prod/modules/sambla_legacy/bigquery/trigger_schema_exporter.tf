cbd4df49233031bd9989370f44717f80b4a93c42
Aruldharanikumar <aruldharani.kumar@samblagroup.com>
2024-12-03 10:22:26 +0100

    restructuring

--- prod/modules/sambla_legacy/bigquery/trigger_schema_exporter.tf
+++ prod/modules/sambla_legacy/bigquery/trigger_schema_exporter.tf
@@ -1,8 +1,8 @@
 # Trigger Python script to generate schema files after data transfer is done
 resource "null_resource" "generate_schemas" {
   provisioner "local-exec" {
-    command     = "python3 /Users/aruldharani/Sambla/de-ingestion-staging-layer-1/prod/policy_tags_service/policy_assignment/policy_tags_sambla_legacy.py"
-    working_dir = "/Users/aruldharani/Sambla/de-ingestion-staging-layer-1/prod/policy_tags_service/policy_assignment"
+    command     = "python3 /Users/aruldharani/Sambla/de-ingestion-staging-layer-1/prod/policy_tags_service/policy_assignment/sambla_legacy/policy_tags_sambla_legacy.py"
+    working_dir = "/Users/aruldharani/Sambla/de-ingestion-staging-layer-1/prod/policy_tags_service/policy_assignment/sambla_legacy"
   }
 
   depends_on = [
--- prod/modules/sambla_legacy/bigquery/trigger_schema_update.tf
+++ prod/modules/sambla_legacy/bigquery/trigger_schema_update.tf
@@ -1,8 +1,8 @@
-# Trigger Python script to generate schema files after data transfer is done
+# Trigger Python script to update schemas
 resource "null_resource" "update_schemas" {
   provisioner "local-exec" {
-    command     = "python3 /Users/aruldharani/Sambla/de-ingestion-staging-layer-1/prod/policy_tags_service/policy_assignment/update_sambla_legacy_table_schema.py"
-    working_dir = "/Users/aruldharani/Sambla/de-ingestion-staging-layer-1/prod/policy_tags_service/policy_assignment"
+    command     = "python3 /Users/aruldharani/Sambla/de-ingestion-staging-layer-1/prod/policy_tags_service/policy_assignment/sambla_legacy/update_sambla_legacy_table_schema.py"
+    working_dir = "/Users/aruldharani/Sambla/de-ingestion-staging-layer-1/prod/policy_tags_service/policy_assignment/sambla_legacy"
   }
 
   depends_on = [
