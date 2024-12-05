499fbe8fe5eed8e71ab36ea3888779a00c33a7de
duygugencc <duygu.genc@samblagroup.com>
2024-12-05 14:23:33 +0100

    "rebasing to maxwell"

--- prod/modules/auth_views/locals.tf
+++ prod/modules/auth_views/locals.tf
@@ -10,7 +10,7 @@ locals {
 
     }
   })
-    unencrypted_schema_table_queries = tomap({
+  unencrypted_schema_table_queries = tomap({
     for line in split("\n", trimspace(file("../prod/authorized_view_service/templates/auth_view_mapping_non_encrypted.txt"))) :
     "${split("|", line)[0]}.${split("|", line)[1]}" => {
       schema = split("|", line)[0]
