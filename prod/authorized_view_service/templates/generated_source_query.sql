eb51c7951ebce3b30d5132b3338965d091da1d4b
Aruldharanikumar <aruldharani.kumar@samblagroup.com>
2024-12-05 15:09:19 +0100

    auth view service refactoring

--- prod/authorized_view_service/templates/generated_source_query.sql
+++ prod/authorized_view_service/templates/generated_source_query.sql
@@ -1,5 +1,9 @@
 SELECT * FROM `sambla-data-staging-compliance.lvs_integration_legacy`.INFORMATION_SCHEMA.COLUMNS
 UNION ALL 
+SELECT * FROM `sambla-data-staging-compliance.maxwell_integration_legacy`.INFORMATION_SCHEMA.COLUMNS
+UNION ALL 
 SELECT * FROM `sambla-data-staging-compliance.salus_integration_legacy`.INFORMATION_SCHEMA.COLUMNS
 UNION ALL 
 SELECT * FROM `sambla-data-staging-compliance.rahalaitos_integration_legacy`.INFORMATION_SCHEMA.COLUMNS
+UNION ALL 
+SELECT * FROM `sambla-data-staging-compliance.sambla_legacy_integration_legacy`.INFORMATION_SCHEMA.COLUMNS
