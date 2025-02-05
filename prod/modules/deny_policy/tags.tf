17d045a3559b60e1685b636af5450b98a65bf96f
Aruldharanikumar <161830112+Aruldharanikumar@users.noreply.github.com>
2025-02-05 17:11:23 +0100

    Deny policy Go live changes (#42)
    * deny_policy final
    
    * fixes

--- prod/modules/deny_policy/tags.tf
+++ prod/modules/deny_policy/tags.tf
@@ -1,17 +1,17 @@
 # Create a Tag Key
-resource "google_tags_tag_key" "tag_key_prod" {
-  short_name = "gdpr_complaince_tag_test"
+resource "google_tags_tag_key" "tag_key_prod_live" {
+  short_name = "gdpr_complaince_tag_prod"
   description  = "This Tag key is for tagging datasets and tables for GDPR purposes"
-  parent       = "projects/${var.project_id}"
+  parent       = "projects/${var.data_domain_project_id}"
   lifecycle {
     prevent_destroy = true  # Prevents Terraform from destroying this resource
   }
 }
 
 # Create a Tag Value for the created Tag Key
-resource "google_tags_tag_value" "tag_value_prod" {
-  parent = "tagKeys/${google_tags_tag_key.tag_key_prod.name}"
-  short_name   = "gdpr_complaince_5year_test"
+resource "google_tags_tag_value" "tag_value_prod_live" {
+  parent = "tagKeys/${google_tags_tag_key.tag_key_prod_live.name}"
+  short_name   = "gdpr_complaince_5year_prod"
   description = "This Tag value is for tagging datasets and tables for GDPR purposes"
   lifecycle {
     prevent_destroy = true  # Prevents Terraform from destroying this resource
