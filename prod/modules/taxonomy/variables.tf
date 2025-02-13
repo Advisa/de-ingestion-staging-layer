b948ac778e88876e81f2f98d6635b1ef27e0059b
duygugencc <duygu.genc@samblagroup.com>
2025-02-13 15:28:32 +0100

    iam permissions added for taxonomy tables access

--- prod/modules/taxonomy/variables.tf
+++ prod/modules/taxonomy/variables.tf
@@ -9,3 +9,10 @@ variable "region" {
   type        = string
 }
 
+variable "policy_dataset_id" {
+  description = "The dataset name for policy tags metadata tables"
+  type        = string
+  default = "sambla-data-staging-compliance.policy_tags_metadata"
+
+  
+}
