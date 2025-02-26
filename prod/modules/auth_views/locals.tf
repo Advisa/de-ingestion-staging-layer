00ce3dc346e6a01ebb3ed0fc0393f9a199ab3fdd
duygugencc <duygu.genc@samblagroup.com>
2025-02-26 15:37:55 +0100

    rahalaitos auth views added to the module

--- prod/modules/auth_views/locals.tf
+++ prod/modules/auth_views/locals.tf
@@ -46,6 +46,17 @@ locals {
 
     }
   })
+
+  rahalaitos_schema_table_queries = tomap({
+    for line in split("\n", trimspace(file("../prod/authorized_view_service/templates/auth_view_mapping_rahalaitos.txt"))) :
+    "${split("|", line)[0]}.${split("|", line)[1]}" => {
+      schema = split("|", line)[0]
+      table  = split("|", line)[1]
+      query  = split("|", line)[2]
+
+    }
+  })
+
   unencrypted_schema_table_queries = tomap({
     for line in split("\n", trimspace(file("../prod/authorized_view_service/templates/auth_view_mapping_non_encrypted.txt"))) :
     "${split("|", line)[0]}.${split("|", line)[1]}" => {
