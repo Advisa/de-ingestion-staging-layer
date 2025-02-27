c91f68cce017cbfce8f75ab6710a5cb7dbc37074
duygugencc <duygu.genc@samblagroup.com>
2025-02-27 12:44:03 +0100

    Rahalaitos Auth views (#64)
    * rahalaitos auth views generated
    
    * auth view definition added for rahalaitos
    
    * rahalaitos auth views added to the module
    
    * Polict tag addition (#67)
    
    * finnish city policy tag addition
    
    * finnish city policy tag addition
    
    * kaupunki is encrypted
    
    ---------
    
    Co-authored-by: Aruldharanikumar <161830112+Aruldharanikumar@users.noreply.github.com>

--- prod/modules/auth_views/main.tf
+++ prod/modules/auth_views/main.tf
@@ -100,3 +100,18 @@ resource "google_bigquery_table" "maxwell_auth_views_test" {
   }
 
 }
+
+# Create rahalaitos auth views in testing dataset
+resource "google_bigquery_table" "rahalaitos_auth_views_test" {
+  for_each = local.rahalaitos_schema_table_queries
+
+  dataset_id         = "auth_view_testing"
+  table_id           = "view_${each.value.table}"
+  deletion_protection = true
+
+  view {
+    query           = each.value.query
+    use_legacy_sql  = false
+  }
+
+}
