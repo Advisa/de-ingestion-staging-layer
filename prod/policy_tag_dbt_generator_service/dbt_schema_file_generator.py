3eeb0dbbc0b81a44f91b834631a73aa770bed139
duygugencc <duygu.genc@samblagroup.com>
2025-02-28 11:55:20 +0100

    Policy tags Edge Cases fix (#68)
    * policy tag assignment & policy tag dbt generator services adjusted to include edge cases
    
    * policy tag assignment service adjusted to include edge cases
    
    * dbt policy tag generator service is updated
    
    * removed unnecessary lines of code
    
    * removed unnecessary lines of code
    
    * taxonomy filter is added to the schema generator file
    
    * correct taxonomy filter is applied

--- prod/policy_tag_dbt_generator_service/dbt_schema_file_generator.py
+++ prod/policy_tag_dbt_generator_service/dbt_schema_file_generator.py
@@ -70,6 +70,7 @@ class BigQuerySchemaExporter:
         FROM `{self.config["source_project"]}.{self.config["source_dataset"]}.{self.config["metadata_table"]}` t1
         JOIN `{self.config["source_project"]}.{self.config["source_dataset"]}.{self.config["taxonomy_table"]}` t2
         ON t1.taxonomy_id = t2.id
+        WHERE taxonomy_id IN ('7698000960465061299', '8248486934170083143', '655384675748637071')
         """
         query_job = self.client.query(query)
         result = query_job.result()
@@ -84,7 +85,7 @@ class BigQuerySchemaExporter:
         # Create policy_tag_mapping for taxonomy_name and corresponding tag reference
         policy_tag_mapping = {}
         for _, row in policy_metadata.iterrows():
-            tag_name = row["display_name"].lower()
+            tag_name = self.normalize_name(row["display_name"])
             tag_suffix = row["policy_tag_id"]
             policy_tag_name = row["taxonomy_name"]
 
@@ -94,6 +95,10 @@ class BigQuerySchemaExporter:
             policy_tag_mapping[tag_name] = tag_link
 
         return policy_tag_mapping
+    
+    def normalize_name(self, name):
+        """Normalize the name by converting to lowercase and removing underscores."""
+        return name.lower().replace("_", "")
 
     def match_policy_tags_to_fields(self, fields, policy_tag_mapping, parent_name=None):
         """Match policy tags to schema fields."""
@@ -104,6 +109,9 @@ class BigQuerySchemaExporter:
                 continue
 
             field_name = field["name"]
+            field_type = field["type"]
+            normalized_field_name = self.normalize_name(field_name)
+            
 
             if field.get('fields'):
                 nested_fields = self.match_policy_tags_to_fields(
@@ -111,10 +119,24 @@ class BigQuerySchemaExporter:
                 )
                 field["fields"] = nested_fields
             else:
-                field_name_lower = field_name.lower()
-                if field_name_lower in policy_tag_mapping:
-                    tag_link = policy_tag_mapping[field_name_lower]
-                    field["policyTags"] = {"names": [tag_link]}
+                # Explicitly check for ssn and balance when they are not of type STRING
+                if (normalized_field_name == "ssn" or normalized_field_name == "nationalid" or normalized_field_name == "validnationalid") and field_type != "STRING" and field_type != "BOOL":
+                    field["policyTags"] = {
+                        "names": [
+                            "projects/sambla-data-staging-compliance/locations/europe-north1/taxonomies/7698000960465061299/policyTags/8190767684129261300"
+                        ]
+                    }
+                elif normalized_field_name == "phonenumber" and field_type != "STRING":
+                    field["policyTags"] = {
+                        "names": [
+                            "projects/sambla-data-staging-compliance/locations/europe-north1/taxonomies/7698000960465061299/policyTags/1553289368757892144"
+                        ]
+                    }
+
+                else:
+                    if normalized_field_name in policy_tag_mapping:
+                        tag_link = policy_tag_mapping[normalized_field_name]
+                        field["policyTags"] = {"names": [tag_link]}
 
             if parent_name and not field.get('name').startswith(f"{parent_name}."):
                 field["name"] = f"{parent_name}.{field['name']}"
--- prod/policy_tags_service/policy_assignment/helpers.py
+++ prod/policy_tags_service/policy_assignment/helpers.py
@@ -64,7 +64,7 @@ class PolicyAssignmentService:
         query = f"""
         SELECT taxonomy_id, display_name, policy_tag_id
         FROM `{self.project_id}.{self.policy_tags_table}`
-        WHERE taxonomy_id IN ('462501529798891334', '1348545653474742340', '8452725999489655507')
+        WHERE taxonomy_id IN ('7698000960465061299', '8248486934170083143', '655384675748637071')
         """
         query_job = self.client.query(query)
         result = query_job.result()
@@ -87,6 +87,7 @@ class PolicyAssignmentService:
 
         for field in fields:
             field_name = field["name"]
+            field_type = field["type"]
             normalized_field_name = self.normalize_name(field_name)
 
             # Handle nested fields recursively
@@ -96,19 +97,34 @@ class PolicyAssignmentService:
                 )
                 field["fields"] = nested_fields
             else:
-                # Find matching tag in policy_tags
-                matching_tag = policy_tags[policy_tags['normalized_display_name'] == normalized_field_name]
-                
-
-                if not matching_tag.empty:
-                    policy_tag = matching_tag['policy_tag_id'].values[0]
-                    taxonomy_id = matching_tag['taxonomy_id'].values[0]  # Dynamically select taxonomy
-
+                # Explicitly check for ssn and balance when they are not of type STRING
+                if (normalized_field_name == "ssn" or normalized_field_name == "nationalid" or normalized_field_name == "validnationalid") and field_type != "STRING" and field_type != "BOOL":
                     field["policyTags"] = {
                         "names": [
-                            f"projects/{self.project_id}/locations/europe-north1/taxonomies/{taxonomy_id}/policyTags/{policy_tag}"
+                            "projects/sambla-data-staging-compliance/locations/europe-north1/taxonomies/7698000960465061299/policyTags/8190767684129261300"
                         ]
                     }
+                elif normalized_field_name == "phonenumber" and field_type != "STRING":
+                    field["policyTags"] = {
+                        "names": [
+                            "projects/sambla-data-staging-compliance/locations/europe-north1/taxonomies/7698000960465061299/policyTags/1553289368757892144"
+                        ]
+                    }
+
+                else:
+                    # Find matching tag in policy_tags
+                    matching_tag = policy_tags[policy_tags['normalized_display_name'] == normalized_field_name]
+
+
+                    if not matching_tag.empty:
+                        policy_tag = matching_tag['policy_tag_id'].values[0]
+                        taxonomy_id = matching_tag['taxonomy_id'].values[0]  # Dynamically select taxonomy
+
+                        field["policyTags"] = {
+                            "names": [
+                                f"projects/{self.project_id}/locations/europe-north1/taxonomies/{taxonomy_id}/policyTags/{policy_tag}"
+                            ]
+                        }
 
             # Ensure proper parent-child field naming
             if parent_name and not field.get('name').startswith(f"{parent_name}."):
@@ -179,7 +195,6 @@ class PolicyAssignmentService:
         try:
     
             policy_tags = self.fetch_policy_tags()
-            print(policy_tags)
 
             self.construct_iam_policies(policy_tags)
 
