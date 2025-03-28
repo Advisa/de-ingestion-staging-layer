dd47d898a05fc552a0c5ac75a8b9e0d78c48efa8
duygugencc <duygu.genc@samblagroup.com>
2025-03-28 14:48:40 +0100

    external table source format fix

--- prod/modules/rahalaitos/bigquery/main.tf
+++ prod/modules/rahalaitos/bigquery/main.tf
@@ -14,7 +14,10 @@ resource "google_bigquery_dataset" "rahalaitos_dataset" {
 locals {
   table_mappings = tomap({
     for line in split("\n", trimspace(file("${path.module}/rahalaitos_external_table_info.txt"))) :
-    split(",", line)[0] => split(",", line)[1]
+    split(",", line)[0] => {
+      gcs_path = split(",", line)[1]
+      source_format = split(",", line)[3]
+    }
   })
 }
 
@@ -23,14 +26,14 @@ resource "google_bigquery_table" "external_tables" {
   # iterate over the local variable to get the table_name and gcs bucket path for each line in the txt file
   for_each = local.table_mappings 
   dataset_id                = google_bigquery_dataset.rahalaitos_dataset.dataset_id
-  # 'each.key' refers to the table name, and 'each.value' refers to the GCS location.
+  # 'each.key' refers to the table name,'each.value.source_format' refers to the source file format and 'each.value.gcs_path' refers to the GCS location.
   table_id                  = "${each.key}"
   deletion_protection       = true
     external_data_configuration {
     autodetect    = false
-    source_format = "NEWLINE_DELIMITED_JSON"
+    source_format = each.value.source_format
     connection_id = var.connection_id
-    source_uris   = [each.value]
+    source_uris   = [each.value.gcs_path]
   }
     # must to define a schema when we create a table
     schema = file("schemas/rahalaitos/${each.key}_schema.json")
